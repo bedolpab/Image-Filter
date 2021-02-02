38bdcaa9b3e2048642f173c8e733479c14ab998d
Pablo Bedolla <70508631+PabloCompSci@users.noreply.github.com>
2021-02-01 22:13:46 -0600

    Update info_start.py

--- info_start.py
+++ info_start.py
@@ -3,7 +3,6 @@ from termcolor import colored
 
 github = (colored('github.com/PabloCompSci/', 'blue'))
 def info():
-  error = (colored('Entering an incorrect character may result in a program malfunction', 'blue'))
   print("Welcome to Image Filter. This program allows you to apply some of our custom filters to your images.")
   print(f'When selecting a filter, please enter as proposed. {error}')
   print(f"Source code can be found on github: {github} if you would like to fork it or use it yourself")
